20933e324d187f854ca156973df7abfbe8577c4c
muhsin dsktp <muhsincodes@gmail.com>
2024-05-29 20:10:31 +0300

    Added base server related configs

---
+++ server.js
@@ -0,0 +1,22 @@
+//all things server related
+const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+  path: path.resolve(`${__dirname}/config.env`),
+});
+
+const PORT = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(console.log('Database connection successful'));
+
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`server running on port ${PORT}`);
+});
