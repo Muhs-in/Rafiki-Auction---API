173c5a54a27757de1912843329e6f6bf8e1b8ea3
muhsin ibrahim <muhsincodes@gmail.com>
2024-06-12 09:32:15 +0300

    implmented an error handler middleware

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 const express = require('express');
 const morgan = require('morgan');
 
+//local modules
+const AppError = require('./utils/appError');
+const errorHandler = require('./controllers/errorController');
+
 const app = express();
 
 //route handlers
 const carRoutes = require('./routes/carRoutes');
 const userRoutes = require('./routes/userRoutes');
 const bidRoutes = require('./routes/bidRoutes');
-const auctionRoutes = require('./routes/carRoutes');
+const auctionRoutes = require('./routes/auctionRoutes');
 
 //middlewares
 app.use(express.json()); //--gives me access to req boyd
@@ -20,5 +24,11 @@ app.use('/api/v4/users', userRoutes);
 app.use('/api/v4/cars', carRoutes);
 app.use('/api/v4/auctions', auctionRoutes);
 app.use('/api/v4/bids', bidRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
 
 module.exports = app;
