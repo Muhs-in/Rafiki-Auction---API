6770aaa393a71ed8164943756334c2a9ee911177
muhsin ibrahim <muhsincodes@gmail.com>
2024-06-12 09:17:58 +0300

    implemented a protected route and authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const { promisify } = require('util');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -7,40 +10,32 @@ const signToken = (id) => {
   });
 };
 
-exports.signup = async (req, res, next) => {
-  try {
-    const newUser = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-    });
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+  });
 
-    const token = signToken(newUser._id);
+  const token = signToken(newUser._id);
 
-    res.status(201).json({
-      status: 'success',
-      token,
-      user: newUser,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  res.status(201).json({
+    status: 'success',
+    token,
+    user: newUser,
+  });
+});
 
-exports.login = async (req, res, next) => {
+exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  //1) Check if email and password actually exist
   if (!email || !password) {
     return res.status(400).json({
       error: 'Please provide email and password',
     });
   }
-  //2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
@@ -49,10 +44,53 @@ exports.login = async (req, res, next) => {
     });
   }
 
-  //3) if everything okay, send token to client
   const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError(`You are not logged in! Please login to get access.`, 401)
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(new AppError(`The user does not exist`, 401));
+  }
+
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
+
+  req.user = currentUser;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
 };
